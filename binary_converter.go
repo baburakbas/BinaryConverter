144a392374e80e6e15e034d5509a97d1440de849
Hasan Kayan <80827760+hasan-kayan@users.noreply.github.com>
2022-08-04 17:28:37 +0300

    Update binary_converter.go

--- binary_converter.go
+++ binary_converter.go
@@ -4,7 +4,7 @@ package main
 import "fmt"
 
 //func divides decimal number to 2 and add the reminder in to digits array until decimal cannot divided
-func binaryConverter(decimalNumber int) {
+func decimalToBinary(decimalNumber int) {
 	var digits []int
 	for decimalNumber != 0 {
 		var integer int = decimalNumber % 2
@@ -19,6 +19,6 @@ func main() {
 	var num int
 	fmt.Println("Enter the number you want to convert :")
 	fmt.Scan(&num)
-	binaryConverter(num)
+	decimalToBinary(num)
 
 }
