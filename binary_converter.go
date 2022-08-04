4abb7aeb54ae6de8cbdcc1127bfb7ce61edc0cfd
Hasan Kayan <80827760+hasan-kayan@users.noreply.github.com>
2022-08-04 17:23:50 +0300

    Add files via upload

---
+++ binary_converter.go
@@ -0,0 +1,24 @@
+// Binary to decimal converter code
+package main
+
+import "fmt"
+
+//func divides decimal number to 2 and add the reminder in to digits array until decimal cannot divided
+func binaryConverter(decimalNumber int) {
+	var digits []int
+	for decimalNumber != 0 {
+		var integer int = decimalNumber % 2
+		decimalNumber = decimalNumber / 2
+		digits[0] = integer
+
+	}
+	fmt.Println(digits)
+}
+
+func main() {
+	var num int
+	fmt.Println("Enter the number you want to convert :")
+	fmt.Scan(&num)
+	binaryConverter(num)
+
+}
