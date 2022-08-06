d7cf91ed128163d4954bb6b68cfbbca158a668ce
Hasan Kayan <80827760+hasan-kayan@users.noreply.github.com>
2022-08-06 21:22:00 +0300

    Update binary_converter.go

--- binary_converter.go
+++ binary_converter.go
@@ -8,11 +8,30 @@ func decimalToBinary(decimalNumber int) {
 	var digits []int
 	for decimalNumber != 0 {
 		var integer int = decimalNumber % 2
-		decimalNumber = decimalNumber / 2
 		digits[0] = integer
+		decimalNumber = decimalNumber / 2
 
 	}
 	fmt.Println(digits)
+}
+func binaryToDecimal(number string) {
+	// Assuming that number contains 0,1s
+	// Used to store result
+	var result int64 = 0
+	var bit int = 0
+	var n int = len(number) - 1
+	// Execute given number in reverse order
+	for n >= 0 {
+		if number[n] == '1' {
+			// When get binary 1
+			result += (1 << (bit))
+		}
+		n = n - 1
+		// Count number of bits
+		bit++
+	}
+	// Display decimal result
+	fmt.Println("  Decimal :  ", result)
 }
 
 func main() {
